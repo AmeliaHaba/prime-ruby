f9b8706ca91d6825d1cb6a194522f345abf5dd65
Amelia Haba <amelia.haba347@gmail.com>
2021-03-01 21:28:15 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    elsif (2...number).any? {|prime_one| number % prime_one == 0 }
+        return false
+    else
+        return true
+    end
+end 
